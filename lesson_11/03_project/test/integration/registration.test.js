398faaab2ec737b6aae20e6728411d3967200fc4
Daniel Marchenko <marchenkodanil97@gmail.com>
2023-08-30 19:34:23 +0300

    lesson 11 upd

--- lesson_11/03_project/test/integration/registration.test.js
+++ lesson_11/03_project/test/integration/registration.test.js
@@ -1,5 +1,5 @@
 require('dotenv').config()
-const request = require('supertest')
+const reqTest = require('supertest')
 const mongoose = require('mongoose')
 const app = require('../../app')
 const User = require('../../models/user')
@@ -18,8 +18,8 @@ describe('registration new user', () => {
     await User.deleteMany()
   })
 
-  it('shoud registrate new user', async () => {
-    const response = await request(app).post('/api/auth/registration').send({
+  it('shoud registrate new user (test db)', async () => {
+    const response = await reqTest(app).post('/api/auth/registration').send({
       email: 'usertest@gmail.com',
       password: '123456',
     })
@@ -27,8 +27,19 @@ describe('registration new user', () => {
     expect(response.body.email).toBe('usertest@gmail.com')
   })
 
+  it.skip('shoud registrate new user (prod db)', async () => {
+    const response = await reqTest('http://localhost:3000')
+      .post('/api/auth/registration')
+      .send({
+        email: 'usertest3@gmail.com',
+        password: '123456',
+      })
+    expect(response.statusCode).toBe(201)
+    expect(response.body.email).toBe('usertest3@gmail.com')
+  })
+
   it('shoud not registrate the same user 2 times', async () => {
-    const response = await request(app).post('/api/auth/registration').send({
+    const response = await reqTest(app).post('/api/auth/registration').send({
       email: 'usertest@gmail.com',
       password: '123456',
     })
@@ -37,6 +48,8 @@ describe('registration new user', () => {
   })
 
   afterAll(async () => {
+    // await User.deleteMany()
+
     await mongoose.disconnect(DB_HOST_TEST).then(() => {
       // console.log('DB disconnected')
     })
